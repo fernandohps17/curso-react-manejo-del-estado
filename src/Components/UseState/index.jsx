a8742b2bb0ee87b875690f8cbb9d6b5f04845210
Fernando Perez <fernandohps17@gmail.com>
2024-11-15 22:17:13 -0400

    Se realizo el estado de carga y estados de mensajes de error

--- src/Components/UseState/index.jsx
+++ src/Components/UseState/index.jsx
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
+
+const SECURITY_CODE = 'paradigma';
 
 function UseState({ name }) {
-
+// verificar error en la clase 6 y clase 5
+    const [value, setValue] = useState('');
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
+    console.log(value)
 
     if (!!loading) {
         useEffect(() => {
-            console.log("Empezando el efecto")
             setTimeout(() => {
                 console.log("Haciendo la validacion")
-                setLoading(false)
+
+                if(value === SECURITY_CODE) {
+                    setLoading(false);
+                } else {
+                    setError(true)
+                    setLoading(false)
+                }
                 console.log("terminando la validacion")
             }, 3000);
-            console.log("Terminando el efecto")
         }, [])
     }
 
@@ -31,7 +39,7 @@ function UseState({ name }) {
                 <p>Cargando...</p>
             )}
 
-            <input placeholder='Codigo de Seguridad' />
+            <input placeholder='Codigo de Seguridad' value={value} onChange={(event) => {setValue(event.target.value)}} />
             <button onClick={() => setLoading(!loading)}>Comprobar</button>
         </div>
     )
